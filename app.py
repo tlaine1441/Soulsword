f4fb524e40e330992a60c474eb5a9b928294940c
Taylor Laine <taylor@taptaprevolution.com>
2021-04-21 21:31:58 -0600

    Clean up

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from models.monster import Monster
-# from actions.breed import bread
-# from traits.genesis import male, female
+from actions.breed import bread
+from traits.genesis import male, female
 
-# monster_one = Monster(owner_id=55, traits=male)
-# monster_two = Monster(owner_id=55, traits=female)
+monster_one = Monster(owner_id=55, traits=male)
+monster_two = Monster(owner_id=55, traits=female)
 
-# breed_data_one = bread(monster_one, monster_two)
+breed_data_one = bread(monster_one, monster_two)
 
-# monster_three = Monster()
+monster_three = Monster()
 
-# print(monster_two.__dict__)
-
-print(Monster)
+print(monster_two.__dict__)
